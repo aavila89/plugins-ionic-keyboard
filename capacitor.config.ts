8640bcfe29605a49c0b5df1ddc1b7085bf597510
aavila89 <90415322+aavila89@users.noreply.github.com>
2023-08-23 19:22:38 -0400

    feat: Added capacitor/keyboard plugin

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,11 +1,19 @@
 import { CapacitorConfig } from '@capacitor/cli';
+import { KeyboardResize, KeyboardStyle } from '@capacitor/keyboard';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'io.keyboard.events',
   appName: 'base',
   webDir: 'www',
   server: {
     androidScheme: 'https'
+  },
+  plugins: {
+    Keyboard: {
+      resize: KeyboardResize.Body,
+      style: KeyboardStyle.Light,
+      resizeOnFullScreen: true,
+    }
   }
 };
 
