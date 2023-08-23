b293484a2e7660750b8ae209d921af5501eaa3e7
aavila89 <90415322+aavila89@users.noreply.github.com>
2023-08-23 19:24:02 -0400

    feat: Implemented events keyboardDidShow and keyboardWillHide
    Added functions showKeyboard and hideKeyboard

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Keyboard  } from '@capacitor/keyboard';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   constructor() {}
+
+  ngOnInit(): void {
+    Keyboard.addListener('keyboardDidShow', info => {
+      document.documentElement.style.setProperty('--margin-keyboard', `-10px`);
+    });
+
+    Keyboard.addListener('keyboardWillHide', () => {
+      document.documentElement.style.setProperty('--margin-keyboard', `-50px`);
+    });
+  }
+  async hideKeyBoard() {
+    await Keyboard.hide();
+  }
 }
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,74 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { Keyboard, KeyboardStyle  } from '@capacitor/keyboard';
+import { IonToast, ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
-  constructor() {}
+  activeStyle: any = KeyboardStyle.Light;
+  toastController = inject(ToastController);
+  constructor() {
+  }
 
+  ngOnInit(): void {
+    Keyboard.addListener('keyboardWillShow', async (info) => {
+      console.log('keyboard will show with height:', info.keyboardHeight);
+      await this.presentToast('Keyboard Will Show ','top');
+    });
+
+    Keyboard.addListener('keyboardWillHide', async () => {
+      await this.presentToast('Keyboard Will Hide ','top');
+    });
+  }
+
+  async showKeyboard() {
+    await this.presentToastEvents('Tab Show Keyboard button.', 'top');
+    const toast = await this.toastController.getTop();
+    toast?.onDidDismiss().then( async () => {
+        await Keyboard.show();
+    });
+  }
+  async hideKeyboard() {
+    await this.presentToastEvents('Tab Hide Keyboard button.', 'top');
+    const toast = await this.toastController.getTop();
+    toast?.onDidDismiss().then( async () => {
+        await Keyboard.hide();
+    });
+  }
+
+  async setStyle() {
+    await this.presentToastEvents('Tab Set Style Keyboard button.', 'top');
+    if (this.activeStyle === KeyboardStyle.Dark) {
+      this.activeStyle = KeyboardStyle.Light;
+    } else {
+      this.activeStyle = KeyboardStyle.Dark;
+    }
+    Keyboard.setStyle({style: this.activeStyle});
+  }
+
+  async presentToast(message: string, position: 'top' | 'middle' | 'bottom') {
+    const toast = await this.toastController.create({
+      color: 'success',
+      message,
+      duration: 1800,
+      position
+    });
+
+    await toast.present();
+  }
+
+  async presentToastEvents(message: string, position: 'top' | 'middle' | 'bottom') {
+    const toast = await this.toastController.create({
+      color: 'medium',
+      message,
+      duration: 1500,
+      position
+    });
+
+    await toast.present();
+  }
 }
